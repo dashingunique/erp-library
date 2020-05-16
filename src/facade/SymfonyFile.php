bc526cefa6fd14c042830d754cac0821992b931e
李奇林 <1107842285@qq.com>
2020-05-16 14:39:43 +0800

    add SymfonyFile.php

--- src/facade/SymfonyFile.php
+++ src/facade/SymfonyFile.php
@@ -17,7 +17,7 @@ use think\Facade;
 /**
  * @method Filesystem copy(string $originFile, string $targetFile, bool $overwriteNewerFiles = false) static 用于拷贝文件。如果目标已经存在，则文件只在源文件的修改日期晚于目标文件时才拷贝。这个行为可以被第三个布尔值参数所覆盖:
  * @method Filesystem mkdir($dirs, int $mode = 0777) static 创建一个目录。在POSIX文件系统上，目录默认被创建为 0777 模式。你可以使用第二个参数来设置自己的mode：
- * @method Filesystem exists(mixed $files) static  检查所有的文件/目录是否存在，如果找不到就返回 false :
+ * @method Filesystem exists(mixed $files) static 检查所有的文件/目录是否存在，如果找不到就返回 false :
  * @method Filesystem touch($files, int $time = null, int $atime = null) 对一个文件设置访问和修改时间。默认使用当前时间。你可以通过第二个参数设置成你自己的。第三个参数是访问时间：
  * @method Filesystem remove(mixed $files) 可以轻松地删除文件, symlinks, 目录等等:
  * @method Filesystem chmod($files, int $mode, int $umask = 0000, bool $recursive = false) 用于改变文件的mode（安全模式）。第四个参数是一个布尔值的递归选项:
